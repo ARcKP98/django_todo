1a6e7d6fc64eb20627f8333316085d92fb3ee554
ARcKP98 <pkartik4386@outlook.com>
2023-04-09 13:32:24 +0000

    Change settings.py.

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -11,10 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from pathlib import Path
 import os
 import dj_database_url
-import env
+
 
 if os.path.isfile("env.py"):
     import env
